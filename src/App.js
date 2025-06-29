46a3afeb8cbe57b8608193d46598bc3333b96bd7
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2025-06-29 22:00:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header.jsx"
 import Body from "./components/Body.jsx"
-
+import About from "./components/About.jsx"
+import { createBrowserRouter , RouterProvider } from "react-router-dom";
 
 
 
@@ -15,8 +16,26 @@ const AppLayout = () =>{
 )
 }
 
+const appRouter = createBrowserRouter([
+    {path: "/",
+        element: <AppLayout/>
+
+    },
+    {
+        path: "/about",
+        element: <About/>,
+
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
+
+
+
+
+
+
 
 
 
