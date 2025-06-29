df87efce0bf6ee4e8d8690a191cdbdc705fba7eb
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2025-06-29 22:01:57 +0530

    Update ResturantCard.jsx

--- src/components/ResturantCard.jsx
+++ src/components/ResturantCard.jsx
@@ -1,4 +1,4 @@
-
+import { CDN_URL } from "../utils/constants";
 
 const ResturantCard = (props) => {
     const {resData} = props
@@ -8,13 +8,14 @@ const ResturantCard = (props) => {
             
             <img className="res-logo" alt = "resturant-logo" 
             src = {
-                "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + 
-                cloudinaryImageId}/>
+                CDN_URL + 
+                cloudinaryImageId}
+                />
             <h4>{name}</h4>
             <h4>{cuisines.join(" , ")}</h4>
             <h4>{avgRating}stars</h4>
             <h4>{costForTwo}</h4>
-            <h4>{deliveryTime}minutes</h4>
+            <h4>{sla.slaString}</h4>
         </div>
     )
 }
