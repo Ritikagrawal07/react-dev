2db911e8a455874b42eb3a11b992cc39521ff585
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2025-06-29 22:01:16 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
 const Header = () => {
+    const[btnNamereact,setbtnNamereact] = useState("login");
+
     return(
         <div className = "header">
             <div className="logo-Container">
@@ -13,11 +16,27 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    
+                      <li>
+                <button 
+             className="login" 
+             onClick = {
+              () => {
+                btnNamereact === "login" ? setbtnNamereact("logut"):setbtnNamereact("login")}
+              }>
+                {btnNamereact}
+                </button>
+                </li>
+                    
                 </ul>
+               
             </div>
         </div>
     )
 }
+
+
+export default Header;
 
 
 export default Header;
