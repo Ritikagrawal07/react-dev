35e1005720c93123d96ccb98ef8b20dd22b1e8b2
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2025-06-29 21:59:42 +0530

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+        <div className="about">
+            <h1>This is about page</h1>
+            <h1>I building great page</h1>
+        </div>
+
+    )
+}
+
+export default About;
